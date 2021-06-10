c20f1d25cec3343fc5bfff2cad0b6746643a56c3
Ceyda Hergul <ceyda_hergul@hotmail.com>
2021-06-10 12:09:21 +0900

    update readme file

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,63 +1,42 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
+const seedProducts = require('../../seeds/product-seeds');
+const seedProductTags = require('../../seeds/product-tag-seeds');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  Product.findAll({
-    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
-    include: [
-        {
-            model: Category,
-            attributes: ['id', 'category_name']
-        },
-        {
-            model: Tag,
-            attributes: ['id', 'tag_name']
-        }
-    ]
-})
-    .then(dbProductData => res.json(dbProductData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
     });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  Product.findOne({
-    where: {
-        id: req.params.id
-    },
-    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
-    include: [
-        {
-            model: Category,
-            attributes: ['id', 'category_name']
-        },
-        {
-            model: Tag,
-            attributes: ['id', 'tag_name']
-        }
-    ]
-})
-    .then(dbProductData => {
-        if (!dbProductData) {
-            res.status(404).json({ message: 'No product found with this id' });
-            return;
-        }
-        res.json(dbProductData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
     });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -70,8 +49,13 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds
+  }).then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -134,24 +118,37 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-  Product.destroy({
-    where: {
-        id: req.params.id
-    }
-})
-    .then(dbProductData => {
-        if (!dbProductData) {
-            res.status(404).json({ message: 'No product found with this id' });
-            return;
-        }
-        res.json(dbProductData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
     });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    
+    res.status(500).json(err);
+  }
+});
+router.post('/seed', (req, res) => {
+  
+
+  res.status(200).json(seedProducts());
+
+});
+router.post('/seed/tag', (req, res) => {
+  
+
+  res.status(200).json(seedProductTags());
+
 });
 
 module.exports = router;
